06429bb6e84f13113ddda790447f214576f4aca4
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-05-23 15:28:02 -0400

    started building objects

---
+++ src/00-objectball.js
@@ -0,0 +1,16 @@
+function gameObject() {
+    return {home: {
+        teamName: 'Brooklyn Nets' ,
+        colors: ['balck', 'white'], 
+        players: {'Alan Anderson': {Number: 0, shoe: 16, Points: 22, },
+                  'Reggie Evans': {Number: 30, shoe: 14, Points: 12, },
+                  'Brook Lopez': {Number: 11, shoe: 17, Points: 17, },
+                  'Mason Plumlee': {Number: 1, shoe: 19, Points: 26, },
+                  'Jason Terry': {Number: 31, shoe: 15, Points: 19, }  
+                }
+    },
+    away: {
+
+    }
+    }
+}
